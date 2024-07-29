3ec14862227d2d3051d1fc6942520e68531e5cf1
yahya <yhylhl.10@gmail.com>
2024-07-29 11:25:41 +0100

    solved admin and user auth , operational

--- src/controllers/auth/authController.js
+++ src/controllers/auth/authController.js
@@ -1,16 +1,15 @@
 const supabase = require("../../config/supabase");
 const jwt = require("jsonwebtoken");
-const bcrypt = require('bcrypt');
+const { rest } = require("../../config/supabase");
 
 
-exports.login = async (req, res) => {
+exports.adminLogin = async (req, res) => {
 	const { email, password } = req.body;
 	try {
 		const { data, error } = await supabase.auth.signInWithPassword({
 			email: email,
 			password: password
 		});
-		console.log(email,password)
 		if (error) {
 			console.error("Error signing in:", error);
 			return res.status(401).json({ error: "Invalid credentials" });
@@ -21,57 +20,42 @@ exports.login = async (req, res) => {
 			.from("Responsable")
 			.select("*")
 			.eq("id", data.user.id)
-			.eq("role", "back-office")
+			.eq("role", "Admin")
 			.single();
 
 		if (adminError || !adminData) {
 			return res.status(403).json({ error: "Access denied" });
 		}
 
-		const token = generateJWT(data.user.id);
-		setResponseHeaders(res);
-		res.status(200).json({ data, token });
+		res.status(200).json({data});
+
 	} catch (err) {
 		console.error("Error during login:", err);
-		res.status(500).json({ error: "Internal server error" });
+		res.status(403).json({error:"Access denied"})
 	}
 };
 
-function generateJWT(userId) {
-	return jwt.sign({ userId }, 'q5$28k3y');
-}
-
-function setResponseHeaders(res) {
-	res.header('Access-Control-Allow-Origin', '*');//this changes in production
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-	res.header('Access-Control-Allow-Credentials', 'true');
-}
 
 
 
-function generateJWT(userId) {
-	return jwt.sign({ userId }, 'q5$28k3y');
-}
-
-
-
-function validateJWT(req, res, next) {
-	const authHeader = req.headers['authorization'];
-	const token = authHeader && authHeader.split(' ')[1];
-
-	if (token == null) {
-		return res.sendStatus(401);
+exports.userLogin = async (req, res) => {
+	const { email, password } = req.body;
+	try {
+		const { data, error } = await supabase.auth.signInWithPassword({
+			email: email,
+			password: password
+		});
+		res.status(200).json({data});
+	} catch (err) {
+		console.error("Error during login:", err);
+		res.status(403).json({err:"Error during login:"});
 	}
+};
 
-	jwt.verify(token, 'q5$28k3y', (err, user) => {
-		if (err) {
-			return res.sendStatus(403);
-		}
-		req.user = user;
-		next();
-	});
-}
+
+
+
+
 
 
 
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth/authController");
 
-router.post("/login", authController.login);
-
+router.post("/adminLogin", authController.adminLogin);
+router.post("/userLogin", authController.valetLogin);
 module.exports = router;
